927c3770f13b020b93c758d0343d58328efc6467
DyerSylvie <sylvierui1@gmail.com>
2023-10-31 13:28:25 -0400

    updating name

--- the-microservices-era/src/components/Home.js
+++ the-microservices-era/src/components/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
 
     return (
         <div>
-            <h1 style={{marginTop: '4%'}}>Rocking Bussin Layered Architecture :)</h1>
+            <h1 style={{marginTop: '4%'}}>Rocking Bussin Microservices Architecture :)</h1>
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', padding: '3rem'}}>
                 {/* Forms directly in the home page for now... maybe tabs or open on button click ? */}
                 <InsertEvent />
